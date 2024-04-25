61ee68dda96fbaf624942f370e2be9a95d056e2e
yash8575 <allamyash0609@gmail.com>
2024-04-25 18:45:09 +0530

    hello script

--- variable.sh
+++ variable.sh
@@ -1,6 +1,7 @@
 #! bin/bash
 
-echo "this is to declare variables"
+variables1:Ramesh
+variables2:suresh
 
-echo "Ramesh:HI suresh"
-echo "Suresh:HI Ramesh"
+echo "$1varaiables1:HI $variables2"
+echo "$variables2:HI $1varaibles1"
